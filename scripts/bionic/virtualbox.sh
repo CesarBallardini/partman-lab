1d5a427819ddaab22acd65e3a3fed11abd89efda
Cesar Ballardini <cesar@ballardini.com.ar>
2019-11-22 15:35:09 -0300

    elimino info para debug

--- scripts/bionic/virtualbox.sh
+++ scripts/bionic/virtualbox.sh
@@ -3,10 +3,6 @@
 SSH_USER=${SSH_USERNAME:-vagrant}
 VBOX_VERSION=$(cat /home/${SSH_USER}/.vbox_version)
 
-echo $VBOX_VERSION es la version a buscar
-ls -lha /home/${SSH_USER}
-sleep 10
-
 mkdir /mnt/vb
 mount -r -o loop /home/${SSH_USER}/VBoxGuestAdditions_${VBOX_VERSION}.iso /mnt/vb
 yes | sh /mnt/vb/VBoxLinuxAdditions.run
